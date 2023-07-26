9871a784648d680eea7c64b60b82937928fb7c6f
hebamsamy <hebasamy655@gmail.com>
2023-07-26 14:34:48 +0300

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" ,redirectTo: "list",pathMatch:"full"},
+  {path:"list",component:CustomerListComponent},
+  {path:"details",component:CustomerDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
